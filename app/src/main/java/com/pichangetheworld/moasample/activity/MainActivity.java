e6ab19685e46a88cf9e326d46887130f5bcf11e4
pchan <pichangetheworld@gmail.com>
2015-02-22 11:54:36 +0900

    Stupid error :(

--- app/src/main/java/com/pichangetheworld/moasample/activity/MainActivity.java
+++ app/src/main/java/com/pichangetheworld/moasample/activity/MainActivity.java
@@ -6,11 +6,11 @@ import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SearchView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
-import android.widget.SearchView;
 import android.widget.TabHost;
 
 import com.pichangetheworld.moasample.R;
